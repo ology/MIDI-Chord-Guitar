80babba8e5f0d2b6d118826f3b7abff9ea00d26a
Gene Boggs <gene@ology.net>
2020-10-27 08:46:14 -0700

    Fix HTML again

--- lib/MIDI/Chord/Guitar.pm
+++ lib/MIDI/Chord/Guitar.pm
@@ -32,7 +32,6 @@ of an C<E A D G B E> tuned guitar.
 =begin html
 
 <p>Here is a handy diagram of MIDI pitch octaves laid out on a guitar neck:</p>
-<br>
 <img src="https://raw.githubusercontent.com/ology/MIDI-Chord-Guitar/main/guitar-position-midi-octaves.png">
 
 =end html
