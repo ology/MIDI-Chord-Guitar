1e2bb6e0096edab3b7fbb6fe6e315dab09722733
Gene Boggs <gene@ology.net>
2020-11-08 04:58:37 -0800

    Rearrange SYNOPSIS

--- lib/MIDI/Chord/Guitar.pm
+++ lib/MIDI/Chord/Guitar.pm
@@ -27,14 +27,14 @@ use namespace::clean;
   my $chord = $mcg->transform('D3', 'dim7', 0);
   # [41, 47, 50, 56]
 
+  # MIDI:
+  #$score->n('wn', @$chord);
+
   my $voicings = $mcg->voicings('dim7');
   # [ [51, 57, 60, 66], [48, 54, 57, 63, 66] ]
 
   $voicings = $mcg->voicings('dim7', 'ISO');
   # [ [D#3 A3 C4 F#4], [C3 F#3 A3 D#4 F#4] ]
-
-  # MIDI:
-  #$score->n('wn', @$chord);
 
 =head1 DESCRIPTION
 
